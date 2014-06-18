079d6a3a5ce078561b063921249aac274fd2c7fd
MussabElDash <mussab14@gmail.com>
2014-06-18 15:45:44 +0300

    add require the of js

--- app/assets/javascripts/appAssets.js
+++ app/assets/javascripts/appAssets.js
@@ -1,4 +1,23 @@
 //= require sign_in
-//= require editor
 //= _solutions
-// require_tree .
+//= assignment_problems
+//= require contests
+//= require courses
+//= require facebook
+//= require helper
+//= require hints
+//= require lecturers
+//= require contests
+//= require problems
+//= require replies
+//= require resources
+//= require solutions
+//= require solutions_constraints
+//= require students
+//= require teaching_assistants
+//= require test_cases
+//= require tips
+//= require topics
+//= require tracks
+//= require utilities
+//= require editor
