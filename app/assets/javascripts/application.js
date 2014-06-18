a2811dd714214a9a2c9d58663a9cd7052ed33904
MussabElDash <mussab14@gmail.com>
2014-06-18 11:30:44 +0300

    remove bootstrap.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,6 @@
 //= require jquery.countdown.js
 //= require bootstrap-wysihtml5/b3
 //= require bootstrap-wysihtml5/locales
-//= require bootstrap
 
 // [User Authentication Advanced - Story 5.9, 5.10, 5.11, 5.14, 5.15]
 // Adds the behaviour of flash messages of the types alert, notice and error
