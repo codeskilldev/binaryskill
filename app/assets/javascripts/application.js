ad83f1858a7908f685f426750317d1bc9fde33a5
MussabElDash <mussab14@gmail.com>
2014-06-18 11:46:31 +0300

    move some javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui-1.10.4
-//= require jquery.bpopup
+//= require jquery.bpopup.min
 //= require jquery.simple-dtpicker
 //= require jquery.tokeninput
 //= require alertify
